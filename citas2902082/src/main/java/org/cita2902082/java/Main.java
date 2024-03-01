a092ad186b60e8411e3dcb24ed9e9ccdbf482f68
GG-Afterdeath <maicoljosue@outlook.com>
2024-02-29 17:39:14 -1200

    OMG que paso

--- citas2902082/src/main/java/org/cita2902082/java/Main.java
+++ citas2902082/src/main/java/org/cita2902082/java/Main.java
@@ -2,6 +2,6 @@ package org.cita2902082.java;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.prIntegerln("Hello world!");
+        System.out.println("Hello world!");
     }
 }
